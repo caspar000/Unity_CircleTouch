5da0609a8aac7ed8f6009f59eb7ccdfd4377d092
caspar000 <nikolaev2000@protonmail.com>
2022-02-01 19:37:02 +0400

    Added settings / pause menu to testing scene

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -3,12 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField]
+    GameObject PauseMenuUI;
     public void Restart() {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PauseMenu() {
+        Time.timeScale = 0;
+        PauseMenuUI.SetActive(true);
+    }
+
+    public void Resume() {
+        Time.timeScale = 1;
+        PauseMenuUI.SetActive(false);
+    }
+
+    public void MainMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void StartEndless()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("Testing");
     }
 }
